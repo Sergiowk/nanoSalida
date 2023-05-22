da0b5ed243eeccf14fde0aff0f653fc65b2c7534
Sergio <sergiowk94@gmail.com>
2023-05-22 19:09:48 +0200

    Api service and headers added

---
+++ src/app/services/api.service.ts
@@ -0,0 +1,25 @@
+import { Injectable } from '@angular/core';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class ApiService {
+  private apiUrl = 'http://ergast.com/api/f1'; 
+
+  constructor(private http: HttpClient) {
+    console.log('Service Drivers')
+   }
+
+  getDrivers() {
+    let head = new HttpHeaders()
+    .set('Type-content', 'aplication/json')
+
+    return this.http.get(`${this.apiUrl}/2023/drivers`, {
+      headers: head
+    });
+    
+  }
+
+
+}
